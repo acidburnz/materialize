ff482b3263fda2482ceba7cae5434081f7599861
acidburnz <acidburnz@xmail.net>
2016-06-14 11:54:13 +0200

    Fix missing files

---
+++ js/materialize.js
@@ -0,0 +1,12 @@
+require(['global', 'animation', 'buttons', 'cards', 'character_counter',
+        'collapsible', 'dropdown', 'forms', 'hammerjs', 'jquery.easing',
+        'jquery.hammer', 'jquery.timeago', 'leanModal', 'materialbox',
+        'parallax', 'picker', 'picker.date', 'pushpin', 'scrollFire',
+        'scrollspy', 'sideNav', 'slider', 'tabs', 'toasts', 'tooltip',
+        'transitions', 'velocity', 'waves'],
+    function (Materialize, Waves) {
+	Materialize.Waves = require('waves');
+	Materialize.Waves.init();
+        return Materialize;
+    }
+);
