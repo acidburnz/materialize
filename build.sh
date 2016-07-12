851a66f96cc5dd9ed01b8ef2c0b82e9cdef52c27
acidburnz <acidburnz@xmail.net>
2016-07-12 11:47:02 +0200

    Cleanup unneeded files Update build.sh added material icons in build

--- build.sh
+++ build.sh
@@ -13,3 +13,29 @@ echo "Build sass Min"
 sass -t compressed sass/materialize.scss:bin/materialize.min.css
 
 echo "Look at bin directory for result"
+
+echo "Build Dist"
+echo "Download meterial design icons"
+if [ -e "fonts/Material-Design-Icons.woff2" ]; then
+  rm fonts/Material-Design-Icons.woff2
+fi
+wget -O fonts/Material-Design-Icons.woff2 https://github.com/google/material-design-icons/blob/master/iconfont/MaterialIcons-Regular.woff2
+
+echo "Copy fonts to dist"
+if [ ! -d "dist/fonts" ]; then
+  mkdir dist/fonts
+fi
+cp fonts/* -R dist/fonts/
+
+echo "Copy JS"
+if [ ! -d "dist/js" ]; then
+  mkdir dist/js
+fi
+cp bin/material*.js dist/js/
+
+echo "Copy CSS"
+if [ ! -d "dist/css" ]; then
+  mkdir dist/css
+fi
+cp bin/material*.css dist/css
+
